2547f079bea71dda771154a6ec023c8abb749065
diana9barrios <dbarrios@miners.utep.edu>
2024-11-02 23:39:32 -0600

    testing

--- code_12.py
+++ code_12.py
@@ -5,10 +5,16 @@ def get_fibonacci_number(position):
     elif position == 1:
         return 1
     else:
-        return ((position-1)+(position-2))
+        # formula fn=fn-1+fn-2
+        for i in range(2, position + 1):
+            return ((position-1)+(position-2))
 
 def get_fibonacci_number_sequence(number):
-    pass #Remove this line and insert your code here. Do not forget to use get_fibonacci_number to create your list of numbers.
+    # pass #Remove this line and insert your code here. Do not forget to use get_fibonacci_number to create your list of numbers.
+    if number == 0:
+        return [0]
+    elif number == 1:
+        return [1]
 
 if __name__ == "__main__":
     # pass #Remove this line and insert your code to test your Fibonacci function here
